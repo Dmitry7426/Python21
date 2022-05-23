99988fa1c30d73f359658a2af09560c48ac32eb2
Dmitry Elkin <dm7426@gmail.com>
2022-05-23 21:15:03 +0500

    final lection

--- Lection23052022/scrypt1.py
+++ Lection23052022/scrypt1.py
@@ -1,1 +1,48 @@
+ # циклы FOR
+
+ # Вводим количенство целых чисел. Выведите максимум из них
+
+# s = 0
+# a = 0
+# for n in input().split():
+#     if int(n) > a:
+#         a = int(n)
+#         print(a)
+
+# Дано число N,
+
+# s = 0
+# n = int(input())
+# for k in range(1, n + 1):
+#     s += k ** k
+# print(s)
+#
+
+# подается список числе, найти среднее арифметическое всех чисел, которые делятся на 3
+
+# s = 0
+# a = 0
+# k = 0
+# for n in input().split():
+#     if int(n) % 3 == 0:
+#         s += int(n)
+#         a += 1
+# k = s / a
+# print(k)
+
+# print (5, end = ', ')
+# print(6)
+
+# вывести таблицу умножения от 1 до 10
+# e = 0
+# for n in (range(1, 10)):
+#
+#     for i in (range(1, 10)):
+#         e = i * n
+#         print(e, end = '  ')
+#     print()
+
+# вывести все строки состоящие из символов a b c d
+
+# alphabet = 'abcd'
 
